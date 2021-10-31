2ce595cb3ba3bdd69ef8f985e58d9bad3f9dead6
ANNA8020 <limJE2005@gmail.com>
2021-10-31 17:21:17 +0900

    오류 + 보안

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 import express from "express";
 
-const app = express;
+const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
-app.use("public", __dirname + "/public");
+app.use("public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
+app.get("/*",(req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening to http://localhost:3000`);
 app.listen(3000, handleListen);
